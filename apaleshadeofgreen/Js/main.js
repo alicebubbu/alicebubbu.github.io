680570d82987554b75b33465d8a3c1f3c5e9602a
Alice Huang <huangluyin@hotmail.com>
2016-01-24 01:22:03 +1100

    added media query and updated category images

--- apaleshadeofgreen/Js/main.js
+++ apaleshadeofgreen/Js/main.js
@@ -59,49 +59,33 @@ Animated scrolling to selected section in nav bar
        1000);
     };
 
+/* ---------------------------------------------------------------------
 
-    //this is where we apply opacity to the arrow
-    $(window).scroll(function(){
+ learn more button on Carousel
 
-      //get scroll position
-      var topWindow = $(window).scrollTop();
-      //multipl by 1.5 so the arrow will become transparent half-way up the page
-      var topWindow = topWindow * 1.5;
-      
-      //get height of window
-      var windowHeight = $(window).height();
-          
-      //set position as percentage of how far the user has scrolled 
-      var position = topWindow / windowHeight;
-      console.log(position);
-      //invert the percentage
-      position = 1 - position;
+--------------------------------------------------------------------- */
+ 
 
-      //define arrow opacity as based on how far up the page the user has scrolled
-      //no scrolling = 1, half-way up the page = 0
-      $('.arrow-wrap').css('opacity', position);
+   $('.button-primary').on('click', gotoAbout);
+        
+   function gotoAbout(e) {
+       e.preventDefault();
+       
+       var $this = $(this);
+       var elemId = $this.attr('href');
+       
+       var $scrollToElem = $(elemId);
 
+
+   $('html,body').animate({
+       scrollTop: $scrollToElem.offset().top},
+       1000);
+    };
+    
 });
 
 
 
 
 
- // Hover effect on categories section pictures
-    $('.scroll-down').on('click', gotoAbout);
-            
-       function gotoAbout(e) {
-           e.preventDefault();
-           
-           var $this = $(this);
-           var elemId = $this.attr('href');
-           
-           var $scrollToElem = $(elemId);
-
-
-       $('html,body').animate({
-           scrollTop: $scrollToElem.offset().top},
-           1000);
-        };
-
-  });
+ 
